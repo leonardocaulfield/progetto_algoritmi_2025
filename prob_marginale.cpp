f6c5033508c4c7baa644ecd8a52bd6d02f0f820e
Leonardo Caulfield <leonardocaulfield@MacBook-Air-di-Leonardo.local>
2025-09-04 13:10:55 +0200

    me lo sono fatto correggere ora devo CAPIRLO

--- prob_marginale.cpp
+++ prob_marginale.cpp
@@ -16,29 +16,38 @@ vector<dettagli> creo_parenti(const vector<variabile>& rete,unordered_map<string
 	ip.resize(rete.size());
 
 	for(int i=0;i<ip.size();i++){
+
 		ip[i].card_valori = rete[i].tipi.size();
 		if(!(rete[i].genitori.empty())){
 			int j=0;
+
+
 			for(auto x : rete[i].genitori){
+
 				ip[i].quale_parent.push_back(name_id.at(x));
 				ip[i].cardinalita_val_par.push_back(rete[name_id[x]].tipi.size());
-
 				j++;
 			}
 		}
+
 		else if(rete[i].genitori.empty()){
 			ip[i].quale_parent.clear();
 			ip[i].cardinalita_val_par.clear();
 		}
+
 		ip[i].stride_parent.resize(ip[i].quale_parent.size());
+
 		for(int k=0;k<ip[i].stride_parent.size();k++){
+
 			ip[i].stride_parent[k] = (k == 0 ? 1 : ip[i].stride_parent[k-1] * ip[i].cardinalita_val_par[k-1]);
 		}
 	}
+
 	return ip;
 }
 
 int righe(int ind,const vector<dettagli>& cardi,const vector<int>& a){
+
 	int riga=0;
 	const auto& par = cardi[ind].quale_parent;
 	const auto& str = cardi[ind].stride_parent;
@@ -50,11 +59,29 @@ int righe(int ind,const vector<dettagli>& cardi,const vector<int>& a){
 	return riga;
 }
 
-bool controllo(const vector<dettagli>& par,vector<int>& assig);
+bool controllo(const vector<dettagli>& par,vector<int>& assig,int fissato,const vector<int>& topo,const vector<int>& topo_pos){
+
+	int stop = topo_pos[fissato];         // posizione di fissato nella LISTA
+    // Itera sulle POSIZIONI < stop, mappando al nodo corrispondente
+	for (int p = 0; p < stop; ++p) {
+        int j = topo[p];                  // 'j' è ora l'id del nodo da incrementare
+        if (j == fissato) continue;
+        if (++assig[j] < par[j].card_valori) return true;
+        assig[j] = 0;
+    }
+    return false;
+}
 
 double prob_esatta(int ind,const vector<dettagli>& cardi,const vector<int>& a,const vector<variabile>& rete){
-	int r = righe(ind,cardi,a);
-	return rete[ind].cpt[r][a[ind]];
+
+	if(!(rete[ind].genitori.empty())){
+		int r = righe(ind,cardi,a);
+		return rete[ind].cpt[r][a[ind]];
+	}
+	else if(rete[ind].genitori.empty()){
+		return rete[ind].cpt[0][a[ind]];
+	}
+	return 0;
 }
 
 double joint_probability(const vector<variabile>& bay,const vector<int>& ass,const vector<dettagli>& info_parenti,const vector<int>& topo){
@@ -65,33 +92,42 @@ double joint_probability(const vector<variabile>& bay,const vector<int>& ass,con
 	return ris;
 }
 
-double probabilita_marginale(const vector<variabile>& bayes,const vector<int>& topo,const vector<dettagli>& cardi,string name,string val)
+double probabilita_marginale(const vector<variabile>& bayes,
+                             const vector<int>& topo,
+                             const vector<dettagli>& cardi,
+                             const string& name,
+                             const string& val)
 {
-	vector<int> assing;
-	assing.resize(cardi.size());
+    int idx = -1, val_idx = -1;
+    for (size_t i = 0; i < bayes.size(); ++i) {
+        if (bayes[i].name == name) {
+            idx = static_cast<int>(i);
+            for (size_t t = 0; t < bayes[i].tipi.size(); ++t) {
+                if (bayes[i].tipi[t] == val) { val_idx = static_cast<int>(t); break; }
+            }
+            break;
+        }
+    }
+    if (idx < 0 || val_idx < 0) return 0.0;
 
-	for(int i=0;i<assing.size();i++) assing[i] = 0;
+// prob_marginale.cpp (pezzo già presente, ora coerente con A)
+	vector<int> topo_pos = build_topo_pos(topo);
+	int stop = topo_pos[idx];
 
-	double risultato = 0;
+	vector<int> topo_pref;
+	topo_pref.reserve(stop + 1);
+	for (int p = 0; p <= stop; ++p) topo_pref.push_back(topo[p]); // nodi corretti
 
-	do{
-		risultato += joint_probability(bayes,assing,cardi,topo);
+	vector<int> assig(bayes.size(), 0);
+	assig[idx] = val_idx;
 
-	}while(controllo(cardi,assing));
-	
-	return risultato;
+	double somma = 0.0;
+	do {
+	    somma += joint_probability(bayes, assig, cardi, topo_pref); // moltiplica sui nodi giusti
+	} while (controllo(cardi, assig, idx, topo, topo_pos));
+
+	return somma;
 }
-
-
-bool controllo(const vector<dettagli>& par,vector<int>& assig){
-
-	for(int j=0;j<assig.size();j++){
-		if(++assig[j]<par[j].card_valori) return true;
-		assig[j] = 0;
-	}
-	return false;
-}
-
 
 void marginale(const vector<variabile>& b,const vector<int>& topologico,const vector<dettagli>& cardinalita){
 
--- topologial_sort.cpp
+++ topologial_sort.cpp
@@ -1,37 +1,41 @@
+// topologial_sort.cpp
+#include <algorithm>
+
 using namespace std;
 
-void DFS(const vector<vector<int>>& dag,vector<int>& ord,int u,bool v[],int *cont);
-
-vector<int> topo_sort(const vector<vector<int>>& dag){
-
-	vector<int> ordinato;
-	int n = dag.size();
-	bool v[n];
-	ordinato.resize(n);
-
-	for(int i=0;i<n;i++) v[i] = false;
-
-	int contatore = n-1;
-
-	for(int s=0;s< n;s++){
-		if(!(v[s])){
-			DFS(dag,ordinato,s,v,&contatore);
-		}
-	}
-	return ordinato;
+// DFS che costruisce l'ordine topologico (push su fine visita)
+static void DFS_list(const vector<vector<int>>& dag,
+                     int u,
+                     vector<char>& vis,
+                     vector<int>& order)
+{
+    vis[u] = 1;
+    for (int v : dag[u]) {
+        if (!vis[v]) DFS_list(dag, v, vis, order);
+    }
+    order.push_back(u);  // push dopo i figli
 }
 
-void DFS(const vector<vector<int>>& dag,vector<int>& ord,int u,bool v[],int *cont){
+// Restituisce la lista dei nodi in ordine topologico
+vector<int> topo_sort(const vector<vector<int>>& dag) {
+    int n = (int)dag.size();
+    vector<char> vis(n, 0);
+    vector<int> order;
+    order.reserve(n);
 
-	v[u] = true;
-	for(auto x : dag[u])
-	{
-		auto k = x;
-		if(!(v[k])){
-			DFS(dag,ord,k,v,cont);
-		}
-	}
-	ord[u] = *cont;
-	(*cont)--;
+    for (int s = 0; s < n; ++s) {
+        if (!vis[s]) DFS_list(dag, s, vis, order);
+    }
+
+    reverse(order.begin(), order.end()); // ottieni ordine corretto
+    return order; // esempio: [nodo1, nodo2, nodo3, ...]
 }
 
+// Costruisce la mappa nodo -> posizione nell’ordinamento topologico
+vector<int> build_topo_pos(const vector<int>& topo_list) {
+    vector<int> pos(topo_list.size());
+    for (int i = 0; i < (int)topo_list.size(); ++i) {
+        pos[topo_list[i]] = i;
+    }
+    return pos;
+}
